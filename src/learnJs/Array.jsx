0628e71e6cd645b7c660766c3a53c89a8fe2a00c
周文锦 <zhouwenjin@zhouwenjindeMacBook-Pro.local>
2021-11-25 00:57:51 +0800

    reduce方法学习

--- src/learnJs/Array.jsx
+++ src/learnJs/Array.jsx
@@ -11,6 +11,9 @@ export default function Array() {
  },[])
  //第二个参数，作为temp的初始值,不传第二个参数的话temp对应数组第一项item对应数组第二项,，每次return操作都会被记录在temp中;
  console.log(a,"===>a")//[{a:1,b:"1"},{a:2,b:"2"},{a:3,b:"3"},{a:4,b:"4"},{a:3,b:"3"}]
+
+ const arr = [[1,2,3],[2,3,4]]
+ console.log(arr.flat());//该方法可将二维数组转换为一维数组
   return (
     <div>
       数组reduce方法学习
